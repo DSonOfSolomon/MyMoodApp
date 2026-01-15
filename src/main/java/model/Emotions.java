b96dc399eb7e611f65707cf47a878ffc40359bc5
DChioma(D.sonofSolomon) <122977720+DannyCanary@users.noreply.github.com>
2026-01-15 13:42:59 +0000

    Add Emotions enum in model layer for user moods

---
+++ src/main/java/model/Emotions.java
@@ -0,0 +1,45 @@
+package model;
+
+public enum Emotions {
+
+    //Positive emotions
+    HAPPY("😊"),
+    CALM("😌"),
+    MOTIVATE("💪"),
+    HOPEFUL("🌟"),
+
+    //Neutral emotions
+    INDIFFERENT("😐"),
+
+    //Negative emotions
+    SAD("😢"),
+    ANXIOUS("😰"),
+    IRRITABLE("😒"),
+    TIRED("😴"),
+    OVERWHELMED("");
+
+
+    //The emoji associated with the emotion
+    private final String emoji;
+
+    //Constructor
+    Emotions(String emoji) {
+        this.emoji = emoji;
+    }
+
+    /**
+     * @return Emoji string
+     */
+    public String getEmoji() {
+        return emoji;
+    }
+
+    @Override
+    public String toString() {
+        String lower = name().toLowerCase();
+        return Character.toUpperCase(lower.charAt(0))
+                + lower.substring(1) + " " + emoji;
+    }
+}
+
+
