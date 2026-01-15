e3ce4319ac5a68819d93e7ad204d13b4ccb9e674
DChioma(D.sonofSolomon) <122977720+DannyCanary@users.noreply.github.com>
2026-01-15 03:47:50 +0000

    fix: adjust main app

--- src/main/java/moodchecker/MyMoodApp.java
+++ src/main/java/moodchecker/MyMoodApp.java
@@ -5,14 +5,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- * Main launcher for MoodApp.
- * Starts the JavaFX application.
- */
+
 public class MyMoodApp extends Application {
 
     public static void main(String[] args) {
-        launch(args); // starts JavaFX
+        launch(args);
     }
 
     @Override
@@ -20,8 +17,8 @@ public class MyMoodApp extends Application {
         StackPane root = new StackPane();   // blank root
         Scene scene = new Scene(root, 600, 400);
 
-        stage.setTitle("MoodApp");
+        stage.setTitle("Welcome to MoodApp 🤝");
         stage.setScene(scene);
-        stage.show();  // display window
+        stage.show();
     }
 }
